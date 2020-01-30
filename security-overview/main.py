e768edc4cbf0b1fd9964ec9dfa51a196facb5e65
Geir Emblemsvag <geiremb@gmail.com>
2020-01-30 20:52:42 +0100

    Further possibilities.

--- security-overview/main.py
+++ security-overview/main.py
@@ -15,6 +15,9 @@ FILES = [
     # 'alp-stb-006fp_deployment_cms-prod.txt',
 ]
 
+
+# pool = HTTPSConnectionPool(HOST, maxsize=1)
+
 # g = git.cmd.Git(git_dir)
 # g.pull()
 
@@ -25,28 +28,42 @@ FILES = [
 #     raise Exception
 
 
-def handle_docker_image(config, section):
-    print(config[section]['resource.docker.name'])
+def handle_docker_image(section):
+    return {
+        'name': section['resource.docker.name'],
+        'tag': section['resource.docker.version'],
+        'type': 'docker',
+    }
 
 
-def handle_artifact(config, section):
-    print(config[section]['resource.maven.war.groupId'])
+def handle_artifact(section):
+    return {
+        'groupId': section['resource.maven.war.groupId'],
+        'artifactId': section['resource.maven.war.artifactId'],
+        'version': section['resource.maven.war.version'],
+        'type': section['resource.maven.war.type'],
+        'static': True if 'resource.maven.war.isStatic' in section else False,
+    }
 
 
 def main():
-    # pool = HTTPSConnectionPool(HOST, maxsize=1)
     for file in FILES:
+        environment = file.split('_')[2].replace('.txt', '')
         config = configparser.ConfigParser(strict=False)
-        # config.read_string(load_file(pool, FILES_PATH + file))
         with open(FILES_PATH + file, 'r') as inimage:
             config.read_string(inimage.read())
-        # config.read_file(FILES_PATH + file)
+        description = {
+            'environment': environment,
+            'repo_url': config['DEFAULT']['repo.release']
+        }
         for section in config.sections():
+            if section == 'DEFAULT':
+                continue
             if 'resource.maven.war.groupId' in config[section]:
-                handle_artifact(config, section)
+                print({**description, **(handle_artifact(config[section]))})
                 continue
             if 'resource.docker.name' in config[section]:
-                handle_docker_image(config, section)
+                print({**description, **(handle_docker_image(config[section]))})
                 continue
             print(f'[NOPE for {section}!')
 
